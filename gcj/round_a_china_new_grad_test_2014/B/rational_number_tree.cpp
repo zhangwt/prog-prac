386bf95e009a6f48b516cf385b6f627f1a326e82
zhangwt@macpro <zhangwt1989@gmail.com>
2015-03-25 01:23:44 +0800

    shorten variable name

--- gcj/round_a_china_new_grad_test_2014/B/rational_number_tree.cpp
+++ gcj/round_a_china_new_grad_test_2014/B/rational_number_tree.cpp
@@ -8,8 +8,8 @@ using namespace std;
 ifstream fin{"B-small-practice.in"};
 ofstream fout{"B-small-practice.out"};
 
-vector<uint64_t> right_most_in_left(64, 0);
-vector<uint64_t> left_most_in_left(64, 0);
+vector<uint64_t> rmil(64, 0); //ids of right most in left
+vector<uint64_t> lmil(64, 0); //ids of left most in left
 
 void findnumber(uint64_t n, uint64_t p, uint64_t q)
 {
@@ -20,12 +20,11 @@ void findnumber(uint64_t n, uint64_t p, uint64_t q)
         return;
     }
 
-    int max_height = (upper_bound(right_most_in_left.begin(), right_most_in_left.end(), n)
-            - right_most_in_left.begin()) + 1;
+    int max_height = (upper_bound(rmil.begin(), rmil.end(), n) - rmil.begin()) + 1;
     //cout << "max_height=" << max_height << endl;
     int height;
     bool toleft;
-    if (n >= left_most_in_left[max_height-1])
+    if (n >= lmil[max_height-1])
     {
         height = max_height;
         toleft = true;
@@ -33,7 +32,7 @@ void findnumber(uint64_t n, uint64_t p, uint64_t q)
     else
     {
         height = max_height-1;
-        if (n == right_most_in_left[height-1])
+        if (n == rmil[height-1])
         {
             toleft = true;
         }
@@ -47,12 +46,12 @@ void findnumber(uint64_t n, uint64_t p, uint64_t q)
     if (toleft)
     {
         q = p+q;
-        n -= left_most_in_left[height-2];
+        n -= lmil[height-2];
     }
     else
     {
         p = p+q;
-        n -= left_most_in_left[height-1];
+        n -= lmil[height-1];
     }
 
     findnumber(n, p, q);
@@ -65,19 +64,19 @@ void findposition(uint64_t p, uint64_t q)
 
 int main()
 {
-    right_most_in_left[0] = 1;
-    right_most_in_left[1] = 2;
+    rmil[0] = 1;
+    rmil[1] = 2;
     for (int i = 2; i <= 63; ++i)
     {
-        right_most_in_left[i] = right_most_in_left[i-1]*2 + 1;
-        //cout << right_most_in_left[i] << " ";
+        rmil[i] = rmil[i-1]*2 + 1;
+        //cout << rmil[i] << " ";
     }
     //cout << endl;
-    left_most_in_left[0] = 1;
+    lmil[0] = 1;
     for (int i = 1; i <= 63; ++i)
     {
-        left_most_in_left[i] = 2*left_most_in_left[i-1];
-        //cout << left_most_in_left[i] << " ";
+        lmil[i] = 2*lmil[i-1];
+        //cout << lmil[i] << " ";
     }
     //cout << endl;
 
